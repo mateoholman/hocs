56d0f5d7a81e7e7b7eda223f9b5305aea9e9eb38
Matthew Holman <mateoholman@gmail.com>
2017-10-12 22:03:29 -0500

    Added context access to the router

--- src/components/require_authentication.js
+++ src/components/require_authentication.js
@@ -3,7 +3,12 @@ import { connect } from 'react-redux';
 
 export default function(ComposedComponent) {
   class Authentication extends Component {
+    static contextTypes = {
+      router: React.PropTypes.object
+    };
+
     render() {
+      console.log(this.context);
       return <ComposedComponent {...this.props} />;
     }
   }
